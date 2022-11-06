f8c32e8ef12f9173efec91ff3d16027af8be1e59
AlejandroValderrama <avalte07@gmail.com>
2022-11-06 16:48:54 +0100

    CSRF Tokens

--- mysite/settings.py
+++ mysite/settings.py
@@ -160,3 +160,5 @@ EMAIL_HOST_PASSWORD = "julio-verne"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://naturadiet-django-production.up.railway.app/']
