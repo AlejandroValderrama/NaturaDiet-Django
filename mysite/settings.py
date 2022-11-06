ea4efef0ac52ceed18ece30764eb9d141a6032b4
AlejandroValderrama <avalte07@gmail.com>
2022-11-06 18:03:23 +0100

    Add media & EmailPassword

--- mysite/settings.py
+++ mysite/settings.py
@@ -154,7 +154,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "Naturadiet2021@gmail.com"
-EMAIL_HOST_PASSWORD = "julio-verne"
+EMAIL_HOST_PASSWORD = os.environ["EMAILPASSWORD"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
